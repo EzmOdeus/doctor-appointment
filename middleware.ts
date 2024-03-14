488f7735675fcaedb54b36b5d0b5cc1ba447fe00
EzmOdeus <shafrax500@gmail.com>
2024-03-14 13:52:21 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -13,5 +13,5 @@ export async function middleware(request: NextRequest) {
  
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher:[ '/detalis/:path*'],
+  matcher:[ '/detalis/:path*','/My-Booking'],
 }
